df762941df4c72569c33db66facd011e8ba813ef
aliciadark <101828407+aliciadark@users.noreply.github.com>
2022-03-25 23:16:30 +0530

    Create __init__.py

---
+++ userge/helpers/__init__.py
@@ -0,0 +1,13 @@
+# for jutsu_tools
+
+from .helping_tools import (
+    capitaled,
+    report_user,
+    time_date_diff,
+    admin_or_creator,
+    admin_chats,
+    get_response,
+    full_name,
+    msg_type,
+    extract_id
+)
